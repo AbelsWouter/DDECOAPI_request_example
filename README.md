51646164d1883f710816ff0f037e800b230502bf
AbelsWouter <wouter.abels@rws.nl>
2022-09-19 16:50:27 +0200

    Added docstrings to scripts

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 This repositorie contains the code to request data via the DD-ECO-API
 
 Extra Information:
- - Limited Documentation of the API is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api-specs
- - Implementation of the API is found here: https://ddecoapi.aquadesk.nl/index.html
- - Syntax for Filtering the API request is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api/blob/main/filtering.md
+    Limited Documentation of the API is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api-specs
+    Implementation of the API is found here: https://ddecoapi.aquadesk.nl/index.html
+    Syntax for Filtering the API request is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api/blob/main/filtering.md
+
+Arguments of a API request query:
+    query_url (str): Select the desired url of the API to get the correct data.
+    query_filter (str, optional): Filtering within API. Defaults to None.
+    skip_properties (str, optional): Properties to skip in response. Defaults to None.
+    api_key (str, optional): API key for identification as company, required for some url's . Defaults to None.
--- ddecoapi_uitvraag_voorbeeld.py
+++ ddecoapi_uitvraag_voorbeeld.py
@@ -1,31 +1,43 @@
-# Imports the datapraser script from the ddecoapidataparser.py file
+"""Uses ddecoapidataparser script to download AquaDesk data via the DD-ECO-API v2"""
 from ddecoapidataparser import dataparser
 
-# ***
-# File: ddecoapi_uitvraag_voorbeeld.py
-# Author: Wouter Abels (wouter.abels@rws.nl)
-# Created: 28/03/22
-# Last modified: 028/03/2022
-# Python ver: 3.9.7
-# ***
+"""
+File: ddecoapi_uitvraag_voorbeeld.py
+Author: Wouter Abels (wouter.abels@rws.nl)
+Created: 28/03/22
+Last modified: 19/09/2022
+Python ver: 3.10.6
+"""
 
-# Limited Documentation of the API is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api-specs
-# Implementation of the API is found here: https://ddecoapi.aquadesk.nl/index.html
-# Syntax for Filtering the API request is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api/blob/main/filtering.md
+"""
+Limited Documentation of the API is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api-specs
+URL's of the API can be found here: https://ddecoapi.aquadesk.nl/index.html
+Syntax for Filtering the API request is found here: https://github.com/DigitaleDeltaOrg/dd-eco-api/blob/main/filtering.md
+"""
 
-# Load data via the DD-ECO-API v2
-# Configure API key (str) ***fill in the key of own organisation***
+"""
+Configure API key
+api_key = (str)
+- Fill in the key of own organization to download measurement data from the AquaDesk.
+"""
 api_key = ''
 
-# Configure API url and call the ddeciapidataparser script
+"""
+Configure API version and call the dataparser class from the ddecoapidataparser script.
+ddecoapi = class(str).
+- Change this if DD-ECO-API version changes.
+"""
 ddecoapi = dataparser('https://ddecoapi.aquadesk.nl/v2/')
 
-# Get filtered dataframe of requested data, in this example only MACEV data after 01-04-2021 is requested and the measurementobjectnames are excluded
-# query_url (str): API endpoint for query
-    #         query_filter (str, optional): Filtering within API. Defaults to None.
-    #         skip_properties (str, optional): Properties to skip in response. Defaults to None.
-    #         api_key (str, optional): API key for identification as company. Defaults to None.
+
+"""
+EXAMPLE 1:
+Get filtered dataframe of requested data, in this example only MACEV data after 01-04-2021 is requested and the measurementobjectnames are excluded.
+"""
 data_query = ddecoapi.parse_data_dump(query_url = 'measurements', query_filter = 'measurementdate:ge:"2021-04-01";taxontype:eq:"MACEV"', skip_properties='measurementobjectname', api_key = api_key)
 
-# Load TWN list data via the DD-ECO-API, in this example query the TWN list for all MACEV species is requested
+"""
+EXAMPLE 2:
+Load TWN-list data via the DD-ECO-API, in this example query the TWN-list for all MACEV species is requested. This works without specific API Key since query_url parameters is openly accessible.
+"""
 twn_query = ddecoapi.parse_data_dump(query_url = 'parameters', query_filter = 'parametertype:eq:"TAXON";taxontype:eq:"MACEV"', api_key = api_key)
